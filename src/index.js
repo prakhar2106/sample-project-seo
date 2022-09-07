b554271d3c4c2185202b4967c256ca798d2b4c94
prakhar gupta <prakhargupta@prakhars-MacBook-Pro.local>
2022-09-07 13:48:59 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ReactSEOMetaTags } from "react-seo-meta-tags";
+import { Helmet } from "react-helmet";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <Helmet>
+              <title>hello</title>
+            </Helmet>
+            {/* <ReactSEOMetaTags
+              website={{
+                title: "about",
+
+                description: "about desc",
+              }}
+              render={(el) => <Helmet>{el}</Helmet>}
+            /> */}
+            <p>hello about</p>
+          </Route>
+          <Route path="/users">
+            <Helmet>
+              <title>hello</title>
+            </Helmet>
+            {/* <ReactSEOMetaTags
+              website={{
+                title: "users title",
+
+                description: "users desc",
+              }}
+              render={(el) => <Helmet>{el}</Helmet>}
+            /> */}
+            <p>hello users</p>
+          </Route>
+          <Route path="/">
+            <Helmet>
+              <title>hello</title>
+            </Helmet>
+            {/* <ReactSEOMetaTags
+              website={{
+                title: "home",
+
+                description: "home desc",
+              }}
+              render={(el) => <Helmet>{el}</Helmet>}
+            /> */}
+            <p>hello yo</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
